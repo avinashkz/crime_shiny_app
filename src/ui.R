4a5f89f6bc39eba092590fe28f3a6c9fb8ca4200
ap26793 <zi.avi7@gmail.com>
2018-01-19 20:24:46 -0800

    Completed home tab

--- src/ui.R
+++ src/ui.R
@@ -8,26 +8,48 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(plotly)
+library(tidyverse)
+library(shinycssloaders)
+library(DT)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+options(spinner.color="#008d4b")
+
+dashboardPage(skin="green",
+
+              
+  dashboardHeader(title = " Visualizing Violent Crimes in US", titleWidth = 300),
+  dashboardSidebar(
+    sliderInput("slider", h3("Filter year", id = "myh3"),
+                min = 1975, max = 2015, value = c(1975, 2015)),
+    radioButtons("radio", h3("Radio buttons"),
+                 choices = list("Total Population" = "total_pop", "Total Crimes" = "violent_crime","Rape" = "rape_sum",
+                                "Assault" = "agg_ass_sum", "Homicide" = "homs_sum", "Robbery" = "rob_sum"),selected = "violent_crime"),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+    uiOutput("cities"),
+    
+    checkboxGroupInput("checkGroup", 
+                       h3("Extra Options"), 
+                       choices = c("View Crime Proportions" = 1, 
+                                      "Remove Trace" = 2, 
+                                      "Display Legend" = 3, 
+                                      "Enlarge Labels" = 4),
+                       selected = c(3)),
+    
+    width = 300
+  ),
+
+  dashboardBody(
+    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "theme.css")),
+    
+    plotlyOutput("geoPlot"),
+    
+    withSpinner(plotlyOutput("linePlot2", height="340")),
+    verbatimTextOutput("selection")
+    
   )
-))
+)
+
+
